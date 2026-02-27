b3385dd207c4f60a7b5d986627c91cbd72833664
berraakman <berraakman16@gmail.com>
2026-02-27 20:59:26 +0300

    Initial Commit

---
+++ My Agenda/My_AgendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_AgendaApp.swift
+//  My Agenda
+//
+//  Created by Berra Akman on 27.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct My_AgendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ My AgendaTests/My_AgendaTests.swift
@@ -0,0 +1,16 @@
+//
+//  My_AgendaTests.swift
+//  My AgendaTests
+//
+//  Created by Berra Akman on 27.02.2026.
+//
+
+import Testing
+
+struct My_AgendaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
